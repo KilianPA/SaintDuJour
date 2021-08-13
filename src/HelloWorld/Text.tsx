d826bc809e83ef9151592fb01100bb704543eea8
Kilian <kilianpasini@hotmail.fr>
2021-08-13 21:36:50 +0200

    first commit3

---
+++ src/HelloWorld/Text.tsx
@@ -0,0 +1,34 @@
+import {spring, useCurrentFrame, useVideoConfig, interpolate} from 'remotion';
+import moment from 'moment';
+
+export const Text: React.FC<{
+	text: string,
+	top: number,
+	fontSize: number
+}> = ({text, top, fontSize}) => {
+	const videoConfig = useVideoConfig();
+
+	const frame = useCurrentFrame();
+	const opacity = interpolate(frame, [0, 20], [0, 1], {extrapolateRight: 'clamp'});
+
+	return (
+		<h1
+			style={{
+				fontFamily: 'Open Sans',
+				fontWeight: 'bolder',
+				fontSize: fontSize,
+				color: "black",
+				textAlign: 'center',
+				position: 'absolute',
+				zIndex: 3,
+				left: '50%',
+				width: '900px',
+				transform: 'translate(-50%, 0)',
+				opacity: opacity,
+				top: top,
+			}}
+		>
+			{text}
+		</h1>
+	);
+};

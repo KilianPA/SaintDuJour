edae1a67825ca18b974ce9939a57cfdbd50bc6dc
Kilian <kilianpasini@hotmail.fr>
2022-10-18 20:31:01 +0200

    Fix

--- cron.js
+++ cron.js
@@ -1,70 +1,82 @@
-const { exec } = require("child_process");
-var sleep = require('sleep');
-var CronJob = require('cron').CronJob;
-var fs = require('fs')
-var path = require('path')
+const {exec} = require('child_process');
+const sleep = require('sleep');
+const {CronJob} = require('cron');
+const fs = require('fs');
+const path = require('path');
 
-
-console.log('Service Running')
-var myArgs = process.argv.slice(2);
+console.log('Service Running');
+uploadVideo();
+const myArgs = process.argv.slice(2);
 if (myArgs.includes('--launch')) {
-    cronJob()
+	cronJob();
 } else {
-    var job = new CronJob('0 0 7 * * *', function() {
-        cronJob()
-    }, null, true, 'Europe/Paris');
-    job.start();
+	const job = new CronJob(
+		'0 0 7 * * *',
+		() => {
+			cronJob();
+		},
+		null,
+		true,
+		'Europe/Paris'
+	);
+	job.start();
 }
 
-function cronJob () {
-    console.log('Run job ' + logTime());
-    removeVideo()
-    //uploadVideo()
+function cronJob() {
+	console.log('Run job ' + logTime());
+	removeVideo();
+	//uploadVideo()
 }
 
-function createVideo () {
-    var videoProcess = exec('node ../tiktokUpload/github.js')
-    videoProcess.stdout.pipe(process.stdout);
-    videoProcess.on('exit', () =>  {
-        try {
-            console.log(fs.existsSync(path.resolve("../tiktokUpload/out.mp4")))
-            if (fs.existsSync(path.resolve("../tiktokUpload/out.mp4"))) {
-                uploadVideo()
-            }
-        } catch (error) {
-            createVideo()
-        }
-    })
-
+function createVideo() {
+	const videoProcess = exec('node ../tiktokUpload/github.js');
+	videoProcess.stdout.pipe(process.stdout);
+	videoProcess.on('exit', () => {
+		try {
+			console.log(fs.existsSync(path.resolve('../tiktokUpload/out.mp4')));
+			if (fs.existsSync(path.resolve('../tiktokUpload/out.mp4'))) {
+				uploadVideo();
+			}
+		} catch (error) {
+			createVideo();
+		}
+	});
 }
 
-function uploadVideo () {
-    exec('node ../tiktokUpload/index.js', (error, stdout, stderr) => {
-        if (error) {
-            console.log(`error: ${error.message}`);
-            return;
-        }
-        if (stderr) {
-            console.log('Vidéo uploadée' + logTime());
-            return;
-        }
-        console.log(`stdout: ${stdout}`);
-    });
+function uploadVideo() {
+	exec('node ../tiktokUpload/index.js', (error, stdout, stderr) => {
+		if (error) {
+			console.log(`error: ${error.message}`);
+			return;
+		}
+		if (stderr) {
+			console.log('Vidéo uploadée' + logTime());
+			return;
+		}
+		console.log(`stdout: ${stdout}`);
+	});
 }
 
-function removeVideo () {
-    exec('sudo rm ../tiktokUpload/out.mp4')
-    exec('sudo rm ./out.mp4.zip')
-    createVideo()
+function removeVideo() {
+	exec('sudo rm ../tiktokUpload/out.mp4');
+	exec('sudo rm ./out.mp4.zip');
+	createVideo();
 }
 
-
-function logTime () {
-    var currentdate = new Date();
-    return " | " + currentdate.getDate() + "/"
-        + (currentdate.getMonth()+1)  + "/"
-        + currentdate.getFullYear() + " "
-        + currentdate.getHours() + ":"
-        + currentdate.getMinutes() + ":"
-        + currentdate.getSeconds();
+function logTime() {
+	const currentdate = new Date();
+	return (
+		' | ' +
+		currentdate.getDate() +
+		'/' +
+		(currentdate.getMonth() + 1) +
+		'/' +
+		currentdate.getFullYear() +
+		' ' +
+		currentdate.getHours() +
+		':' +
+		currentdate.getMinutes() +
+		':' +
+		currentdate.getSeconds()
+	);
 }
